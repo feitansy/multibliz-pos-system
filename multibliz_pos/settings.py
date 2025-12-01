960eaf4fad3f8059720c19cbdb5df92add37a8bf
Multibliz POS <multiblizinternationalcorp@gmail.com>
2025-12-01 10:07:46 +0800

    Add testserver to ALLOWED_HOSTS for testing and remove test file

--- multibliz_pos/settings.py
+++ multibliz_pos/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-iw*h!o%90q+fg1=pu_6y*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() in ('true', '1', 'yes')
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1,testserver').split(',')
 
 # CSRF Trusted Origins
 CSRF_TRUSTED_ORIGINS = [
